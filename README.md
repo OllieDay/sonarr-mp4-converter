72af3d5dded167c96628522146a9126fbed941a1
Ollie Day <OllieDay@users.noreply.github.com>
2018-02-16 19:51:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sonarr-mp4-converter
+Sonarr post processing script to convert episodes to MP4 using FFmpeg.
---
+++ sonarr-mp4-converter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if ! command -v ffmpeg > /dev/null; then
+	>&2 echo "FFmpeg is not installed."
+	exit 1
+fi
+
+if [ ! $sonarr_eventtype ]; then
+	>&2 echo "sonarr_eventtype is not set."
+	exit 1
+fi
+
+# Skip all events except Download
+if [ $sonarr_eventtype != "Download" ]; then
+	exit 0
+fi
+
+if [ ! "$sonarr_episodefile_path" ]; then
+	>&2 echo "sonarr_episodefile_path is not set."
+	exit 1
+fi
+
+if [ ! -f "$sonarr_episodefile_path" ]; then
+	>&2 echo "File $sonarr_episodefile_path not found."
+	exit 1
+fi
+
+destination="${sonarr_episodefile_path%.*}.mp4"
+
+if [ -f "$destination" ]; then
+	>&2 echo "File $destination already exists."
+	exit 1
+fi
+
+if ! ffmpeg -i "$sonarr_episodefile_path" -codec copy "$destination" &> /dev/null; then
+	>&2 echo "Failed to convert $sonarr_episodefile_path"
+	exit 1
+fi
+
+# Remove original file as it's no longer needed
+rm "$sonarr_episodefile_path"
